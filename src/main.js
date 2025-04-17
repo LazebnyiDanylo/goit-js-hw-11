f61581796daf0a6249b55f4e5b4729ca3f3e8b15
LazebnyiDanylo <daniillazebnuy124@gmail.com>
2025-04-17 16:24:09 +0300

    next fix hw

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 import { getImagesByQuery } from './js/pixabay-api';
 import * as gallery from './js/render-functions';
 
 export const form = document.querySelector('.form');
 const input = document.querySelector('[name="search-text"]');
+const imgGallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', submitForm);
 
 function submitForm(event) {
   event.preventDefault();
+
+  gallery.clearGallery();
+
   if (!input.value.trim()) {
     form.reset();
-    gallery.clearGallery();
     return;
   }
   getImagesByQuery(input.value.trim())
     .then(response => {
-      gallery.clearGallery();
       if (response.data.hits.length === 0) {
         gallery.hideLoader();
         return iziToast.error({
